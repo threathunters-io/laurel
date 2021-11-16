0cb269bf7fa642af1e6422bbee01a58fc8aa3e97
Hilko Bengen <bengen@hilluzination.de>
2021-11-16 22:18:08 +0100

    Add parser for sockaddr_* structures

---
+++ src/sockaddr.h
@@ -0,0 +1,27 @@
+#include <sys/socket.h>
+
+#include <linux/atalk.h>
+#include <linux/atm.h>
+#include <linux/ax25.h>
+#include <linux/caif/caif_socket.h>
+#include <linux/can.h>
+#include <linux/dn.h>
+#include <linux/if_alg.h>
+#include <linux/if_packet.h>
+#include <linux/if_pppox.h>
+#include <linux/if_xdp.h>
+#include <linux/in6.h>
+#include <linux/in.h>
+#include <linux/ipx.h>
+#include <linux/l2tp.h>
+#include <linux/llc.h>
+#include <linux/netlink.h>
+#include <linux/nfc.h>
+#include <linux/phonet.h>
+#include <linux/qrtr.h>
+#include <linux/rose.h>
+#include <linux/rxrpc.h>
+#include <linux/tipc.h>
+#include <linux/un.h>
+#include <linux/vm_sockets.h>
+#include <linux/x25.h>
