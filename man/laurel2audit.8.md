95893fcf18efdc1040360cd252463f67a045302f
Hilko Bengen <bengen@hilluzination.de>
2023-10-20 09:39:23 +0200

    Add laurel2audit filter program

---
+++ man/laurel2audit.8.md
@@ -0,0 +1,45 @@
+---
+title: laurel2audit
+section: 8
+header: System Administration Utilities
+footer: laurel 0.5.3
+---
+
+# NAME
+
+laurel2audit(8) -- transform Laurel logs to back to original Linux Audit format
+
+# SYNOPSIS
+
+This is a simple filter that reads logs written by `laurel(8)` and
+outputs Linux Audit logs that the audit tools and `laurel` itself
+should be able to digest.
+
+# NOTES
+
+"Enriched" (i.e. ALL_CAPS) keys in audit records are discarded.
+
+`EXECVE` records are output on one, possibly very long, line.
+
+If `laurel` has transformed `EXECVE` argument lists to single strings
+(`ARGV_STR`), that transformation may have been lossy: There is no way
+to discern space characters as gaps between arguments from space
+characters as part of individual arguments.
+
+An end-of-event (`EOE`) marker is output for every event. This marker
+is not part of the original `audit.log` file, but it has originally
+been transmitted by the kernel and is passed by `auditd(8)` to
+plugins.
+
+# BUGS
+
+- URL-encoded single bytes within strings are not yet handled.
+- Possibly more.
+
+# SEE ALSO
+
+`laurel(8)`, `aulast(8)`, `aulastlog(8)`, `aureport(8)`, `ausearch(8)`, `ausyscall(8)`, `auvirt(8)`
+
+# AUTHORS
+
+- Hilko Bengen <<bengen@hilluzination.de>>
---
+++ src/bin/laurel2audit/main.rs
@@ -0,0 +1,141 @@
+use indexmap::IndexMap;
+
+use serde::Deserialize;
+// use serde::de::Deserialize;
+use serde_json;
+
+use std::io::BufRead;
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum EventValues {
+    Single(Record),
+    Multi(Vec<Record>),
+}
+
+#[derive(Deserialize, Default)]
+struct Record(IndexMap<String, serde_json::Value>);
+
+#[derive(Deserialize)]
+struct Event {
+    #[serde(rename = "ID")]
+    id: String,
+    #[serde(rename = "NODE")]
+    node: Option<String>,
+    #[serde(flatten)]
+    events: IndexMap<String, EventValues>,
+}
+
+// copy form parser.rs
+#[inline(always)]
+fn is_safe_chr(c: u8) -> bool {
+    c == b'!' || (b'#'..=b'~').contains(&c)
+}
+
+fn print_record(typ: &str, r: &Record) {
+    use serde_json::Value;
+
+    for (n, (k, v)) in r.0.iter().enumerate() {
+        if n == 4 && typ == "SYSCALL" {
+            if let Some(Value::Array(a)) = r.0.get("ARGV") {
+                for (n, v) in a.iter().enumerate() {
+                    if let Value::String(s) = v {
+                        if !s.starts_with("0x") {
+                            continue;
+                        }
+                        print!(" a{n}={}", &s[2..]);
+                    }
+                }
+            }
+        }
+        if k == &k.to_uppercase() {
+            continue;
+        }
+        print!(" {k}=");
+        match v {
+            Value::String(s) => {
+                let b: &[u8] = s.as_bytes();
+                if b.iter().cloned().all(is_safe_chr) {
+                    if (b.starts_with(b"0x") || b.starts_with(b"0o"))
+                        && ["SYSCALL", "PATH"].contains(&typ)
+                    {
+                        print!("{}", &s[2..]);
+                    } else {
+                        print!("{v}");
+                    }
+                } else {
+                    b.iter().for_each(|c| print!("{:02X}", c));
+                }
+            }
+            Value::Array(_) => {
+                todo!()
+            }
+            Value::Object(_) => {
+                todo!()
+            }
+            _ => print!("{v}"),
+        };
+    }
+    println!();
+}
+
+fn main() {
+    let stdin = std::io::stdin().lock();
+    for line in stdin.lines() {
+        let line = match line {
+            Ok(l) => l,
+            Err(e) => {
+                println!("error: {e}");
+                continue;
+            }
+        };
+        let ev: Event = match serde_json::from_str(&line) {
+            Ok(v) => v,
+            Err(e) => {
+                println!("error: {e}");
+                continue;
+            }
+        };
+        let msgid = ev.id;
+        let prefix = match ev.node {
+            Some(node) => format!("node={node} msg=audit({msgid}): "),
+            None => format!("msg=audit({msgid}):"),
+        };
+        for (typ, body) in ev.events.iter() {
+            match (typ.as_str(), body) {
+                ("PARENT_INFO", _) => continue,
+                ("EXECVE", EventValues::Single(r)) => {
+                    let mut r2 = Record::default();
+                    match r.0.get("argc") {
+                        None => continue,
+                        Some(n) => r2.0.insert("argc".into(), n.clone()),
+                    };
+                    if let Some(serde_json::Value::String(s)) = r.0.get("ARGV_STR") {
+                        for (n, arg) in s.split(" ").enumerate() {
+                            r2.0.insert(format!("a{}", n), serde_json::Value::String(arg.into()));
+                        }
+                    } else if let Some(serde_json::Value::Array(a)) = r.0.get("ARGV") {
+                        for (n, arg) in a.iter().enumerate() {
+                            r2.0.insert(format!("a{}", n), arg.clone());
+                        }
+                    } else {
+                        continue;
+                    }
+                    print!("type={typ} {prefix}");
+                    print_record(typ, &r2);
+                }
+                (_, EventValues::Single(r)) => {
+                    print!("type={typ} {prefix}");
+                    print_record(typ, &r);
+                }
+                (_, EventValues::Multi(rs)) => {
+                    for r in rs {
+                        print!("type={typ} {prefix}");
+                        print_record(typ, &r);
+                    }
+                }
+            };
+        }
+        println!("type=EOE {prefix} ");
+    }
+}
