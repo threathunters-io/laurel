261560c1733e286ffb776a8691b40ff2555a39c3
Hilko Bengen <bengen@hilluzination.de>
2024-02-13 22:03:23 +0100

    Replace bencher with divan

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,51 +1,36 @@
-#[macro_use]
-extern crate bencher;
-extern crate gperftools;
+use laurel::parser::parse;
 
-use bencher::Bencher;
+use std::hint::black_box;
+
+use divan;
 
 use gperftools::profiler::PROFILER;
 
-// use std::process;
-
-fn measure(bench: &mut Bencher, line: &[u8]) {
-    let line = line.to_vec();
-    bench.iter(|| {
-        for _ in 0..1000 {
-            laurel::parser::parse(line.clone(), false).unwrap();
-        }
-    });
+#[divan::bench]
+fn parse_syscall() {
+    let _ = black_box(
+        parse(Vec::from(&br#"node=asdfghjk type=SYSCALL msg=audit(1615114232.123:45678): arch=c000003e syscall=59 success=yes exit=0 a0=63b29337fd18 a1=63b293387d58 a2=63b293375640 a3=fffffffffffff000 items=2 ppid=1492834 pid=1492836 auid=1000 uid=1000 gid=1000 euid=1000 suid=1000 fsuid=1000 egid=1000 sgid=1000 fsgid=1000 tty=pts1 ses=1 comm="true" exe="/bin/true" key=(null)ARCH=x86_64 SYSCALL=execve AUID="user" UID="user" GID="user" EUID="user" SUID="user" FSUID="user" EGID="user" SGID="user" FSGID="user"
+"#[..]), false));
 }
 
-fn parse_syscall(bench: &mut Bencher) {
-    measure(bench, &br#"node=asdfghjk type=SYSCALL msg=audit(1615114232.123:45678): arch=c000003e syscall=59 success=yes exit=0 a0=63b29337fd18 a1=63b293387d58 a2=63b293375640 a3=fffffffffffff000 items=2 ppid=1492834 pid=1492836 auid=1000 uid=1000 gid=1000 euid=1000 suid=1000 fsuid=1000 egid=1000 sgid=1000 fsgid=1000 tty=pts1 ses=1 comm="true" exe="/bin/true" key=(null)ARCH=x86_64 SYSCALL=execve AUID="user" UID="user" GID="user" EUID="user" SUID="user" FSUID="user" EGID="user" SGID="user" FSGID="user"
-"#[..])
-}
-
-fn parse_execve_short(bench: &mut Bencher) {
-    measure(
-        bench,
-        &br#"node=asdfghjk type=EXECVE msg=audit(1615114232.123:45678): argc=1 a0="true"
+#[divan::bench]
+fn parse_execve_short() {
+    let _ = black_box(parse(
+        Vec::from(
+            &br#"node=asdfghjk type=EXECVE msg=audit(1615114232.123:45678): argc=1 a0="true"
 "#[..],
-    )
+        ),
+        false,
+    ));
 }
-
-benchmark_group!(b, parse_syscall, parse_execve_short);
 
 fn main() {
     laurel::constants::initialize();
-
-    let test_opts = bencher::TestOpts::default();
-    // if let Some(arg) = std::env::args().skip(1).find(|arg| *arg != "--bench") {
-    //     test_opts.filter = Some(arg);
-    // }
-    let mut benches = Vec::new();
-    benches.extend(b());
     PROFILER
         .lock()
         .unwrap()
         .start(format!("{}.prof", std::env::args().next().unwrap()))
         .unwrap();
-    bencher::run_tests_console(&test_opts, benches).unwrap();
+    divan::main();
     PROFILER.lock().unwrap().stop().unwrap();
 }
